7948f9f76040719942e1c2f7332fdf68aafd78e3
Aztraeus <93314054+Aztraeus@users.noreply.github.com>
2024-04-24 23:26:07 -0700

    Lab8

---
+++ Labs/Lab8/Coin.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Coin {
+    public int coinChange(int[] coins, int amount) {
+        int[] amountCoins = new int[amount + 1];
+        Arrays.fill(amountCoins, amount + 1);
+        amountCoins[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    amountCoins[i] = Math.min(amountCoins[i], amountCoins[i - coin] + 1);
+                }
+            }
+        }
+        return amountCoins[amount] <= amount ? amountCoins[amount] : -1;
+    }
+    
+    public static void main(String[] args) {
+        Coin coin = new Coin();
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        System.out.println(coin.coinChange(coins, amount));
+    }
+}
--- Labs/Lab8/coinChange.py
+++ Labs/Lab8/coinChange.py
@@ -1,11 +1,22 @@
-def coinChange(self, coins, amount):
-    amount_coins = [coins]
-    for i in amount_coins:
-        amount -= amount_coins[i]
-        while amount > amount_coins[i]:
-            amount -= amount_coins[i]
-        if amount < 0:
-            amount += amount_coins[i]
-        print(amount)
+from typing import List
+
+class Coin:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        numCoins = len(coins)
+        amountCoins = [amount + 1] * (amount + 1)
+        amountCoins[0] = 0
         
-coinChange([1,2,5], 11)
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if coin <= i:
+                    amountCoins[i] = min(amountCoins[i], amountCoins[i - coin] + 1)
+        
+        if amountCoins[amount] <= amount:
+            return amountCoins[amount]
+        else: 
+            return -1
+    
+coin = Coin()
+coins = [1, 2, 5]
+amount = 11
+print(coin.coinChange(coins, amount))
